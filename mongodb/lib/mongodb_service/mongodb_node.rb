385e6de6ad8b06a8c9e91a98419effa32f5f7bca
anferneeg <agui@vmware.com>
2011-07-27 14:26:37 +0800

    wait mongod process to quit in case provisioned_service.kill does not wait process which is not node's child
    Change-Id: I57456799f56ca077928c7f2645e1dba8b6be2ccb

--- mongodb/lib/mongodb_service/mongodb_node.rb
+++ mongodb/lib/mongodb_service/mongodb_node.rb
@@ -111,8 +111,17 @@ class VCAP::Services::MongoDB::Node
     ProvisionedService.all.each { |provisioned_service|
       @logger.debug("Try to terminate mongod pid:#{provisioned_service.pid}")
       provisioned_service.kill(:SIGTERM)
+      wait(provisioned_service)
       @logger.debug("mongod pid:#{provisioned_service.pid} terminated")
     }
+  end
+
+  def wait(provisioned_service, interval = 0.2)
+    @logger.debug("wait #{provisioned_service.pid}")
+    while system("ps #{provisioned_service.pid} > /dev/null") do
+      @logger.info("#{provisioned_service.name} still running, pid: #{provisioned_service.pid}}")
+      sleep interval
+    end
   end
 
   def announcement
