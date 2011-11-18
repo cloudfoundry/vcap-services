85ba68693d9c1f09d52ebcd3a5058d26b1e61894
Tang Rui <rtang@vmware.com>
2011-11-17 17:56:32 -0800

    make NATS mbus config optional for gateway-only services
    Change-Id: I1bdb8ffcbc62ef2480475287ade8715ae8dae7a8

--- base/lib/base/base.rb
+++ base/lib/base/base.rb
@@ -32,19 +32,26 @@ class VCAP::Services::Base::Base
     @options = options
     @local_ip = VCAP.local_ip(options[:ip_route])
     @logger.info("#{service_description}: Initializing")
-    @nats_lock = Mutex.new
 
-    NATS.on_error do |e|
-      if e.kind_of? NATS::ConnectError
-        @logger.error("EXITING! NATS connection failed: #{e}")
-        exit
-      else
-        @logger.error("NATS problem, #{e}")
+    @node_nats = nil
+    if options[:mbuts]
+      @nats_lock = Mutex.new
+
+      NATS.on_error do |e|
+        if e.kind_of? NATS::ConnectError
+          @logger.error("EXITING! NATS connection failed: #{e}")
+          exit
+        else
+          @logger.error("NATS problem, #{e}")
+        end
       end
+      @node_nats = NATS.connect(:uri => options[:mbus]) {
+        on_connect_node
+      }
+    else
+      @logger.info("NATS is disabled")
     end
-    @node_nats = NATS.connect(:uri => options[:mbus]) {
-      on_connect_node
-    }
+
     VCAP::Component.register(
       :nats => @node_nats,
       :type => service_description,
@@ -88,7 +95,7 @@ class VCAP::Services::Base::Base
 
   def shutdown()
     @logger.info("#{service_description}: Shutting down")
-    @node_nats.close
+    @node_nats.close if @node_nats
   end
 
   def group_handles_in_json(instances_list, bindings_list, size_limit)
