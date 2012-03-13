36c46f6f0fd8718680873a996a4535c9933ed605
Frank Lu <flu@vmware.com>
2012-03-13 02:51:25 -0600

    backup/dump/import/restore/snapshot/serialize postgresql databases using the parent role rather than superuser
         1. Use the default user (parent role) rather than the super user to backup/dump/import/restore/snapshot/serialize the provisioned database
         2. Add model.rb: list all DataMapper relative classes
         3. Prevent the long-time queries/transctions submmitted by parent roles and super users from being killed
         4. Filter the long-time queries/transactions by postgresql query rather than ruby statements
         5. Now, snapshot/serialization jobs are dummy
    
    Change-Id: Iefed934f5627f0874d4e789cac5a1d6329609f03

--- postgresql/lib/postgresql_service/util.rb
+++ postgresql/lib/postgresql_service/util.rb
@@ -3,7 +3,7 @@ module VCAP
   module Services
     module Postgresql
 
-      # FIXME this should probably go into common
+      # Give various helper functions
       module Util
         VALID_CREDENTIAL_CHARACTERS = ("A".."Z").to_a + ("a".."z").to_a + ("0".."9").to_a
 
@@ -42,6 +42,66 @@ module VCAP
 
         def generate_credential(length=12)
           Array.new(length) { VALID_CREDENTIAL_CHARACTERS[rand(VALID_CREDENTIAL_CHARACTERS.length)] }.join
+        end
+
+        # Return the version of postgresql
+        def pg_version(conn)
+          return -1 unless conn
+          version= conn.query("select version()")
+          reg = /([0~9.]{5})/
+          return version[0]['version'].scan(reg)[0][0][0]
+        end
+
+        # Use this method for backuping and snapshoting the database
+        # name: name of database
+        # host: ip/hostname of your database node
+        # port: port of your database listening
+        # user
+        # passwd
+        # dump_file: the file to store the dumped data
+        # opts: optional arguments
+        #   dump_bin
+        #   logger
+        def dump_database(name, host, port, user, passwd, dump_file, opts = {} )
+          raise "You must provide the following arguments: name, host, port, user, passwd, dump_file" unless name && host && port && user && passwd && dump_file
+
+          dump_bin = opts[:dump_bin] || "pg_dump"
+          dump_cmd = "#{dump_bin} -Fc --host=#{host} --port=#{port} --username=#{user} --file=#{dump_file} #{name}"
+
+          # running the command
+          on_err = Proc.new do |cmd, code, msg|
+            opts[:logger].error("CMD '#{cmd}' exit with code: #{code} & Message: #{msg}") if opts[:logger]  && opts[:logger].respond_to?(error)
+          end
+
+          result = CMDHandle.execute(dump_cmd, nil, on_err )
+          raise "Failed to dump database of #{name}" unless result
+          result
+        end
+
+        # Use this method for restoring and importing the database
+        # name: name of database
+        # host: ip/hostname of your database node
+        # port: port of your database listening
+        # user
+        # passwd
+        # dump_file: the file which stores the dumped data
+        # opts: optional arguments
+        #   restore_bin
+        #   logger
+        def restore_database(name, host, port, user, passwd, dump_file, opts = {} )
+          raise "You must provide the following arguments: name, host, port, user, passwd, dump_file" unless name && host && port && user && passwd && dump_file
+
+          restore_bin = opts[:restore_bin] || pg_restore
+          restore_cmd = "{restore_bin} -h #{host} -p #{port} -U #{user} -d #{name} #{dump_file} "
+
+          # running the command
+          on_err = Proc.new do |cmd, code, msg|
+            opts[:logger].error("CMD '#{cmd}' exit with code: #{code} & Message: #{msg}") if opts[:logger] && opts[:logger].respond_to?(error)
+          end
+
+          result = CMDHandle.execute(restore_cmd, nil, on_err )
+          raise "Failed to restore database of #{name}" unless result
+          result
         end
 
       end
