5b9365eea268849242d3532421177b3803dd9bb3
Yang Yang <yangy@vmware.com>
2012-04-12 04:03:30 -0700

    check vblob status repeatedly for the start of vblob process
    1. added the feature for vblob to check the status of its proces rather
    than waiting for a specific amount of time.
    
    2. added the error file for vblob
    
    3. re-organized some of the code in vblob for http requests,
    code arrangements, etc
    
    Change-Id: I636691f728056f66c4724958321be5201cbf9899

---
+++ vblob/lib/vblob_service/vblob_utils.rb
@@ -0,0 +1,95 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+
+module VCAP
+  module Services
+    module VBlob
+      module Utils
+
+        VBLOB_TIMEOUT = 3
+
+        def gateway_user_operations(operations, options)
+          credentials = "{\"#{options[:username]}\":\"#{options[:password]}\"}"
+          response = nil
+          response = Net::HTTP.start(@local_ip, options[:port]) { |http|
+            http.open_timeout = http.read_timeout = VBLOB_TIMEOUT
+            http.send_request('PUT', operations, credentials, auth_header(options[:admin], options[:adminpass]))
+          }
+          response
+        end
+
+        def service_dir(service_id)
+          File.join(@base_dir, service_id)
+        end
+
+        def log_dir(instance_id)
+          File.join(@vblobd_log_dir,instance_id)
+        end
+
+        def log_file_vblob(instance_id)
+          File.join(log_dir(instance_id), 'vblob.log')
+        end
+
+        def service_exist?(provisioned_service)
+          Dir.exists?(service_dir(provisioned_service.name))
+        end
+
+        def vblob_dir(base_dir)
+          File.join(base_dir,'vblob_data')
+        end
+
+        def auth_header(user,passwd)
+          {"Authorization" => "Basic " + Base64.strict_encode64("#{user}:#{passwd}").strip}
+        end
+
+        def record_service_log(service_id)
+          @logger.warn(" *** BEGIN vblob log - instance: #{service_id}")
+          @logger.warn("")
+          file = File.new(log_file_vblob(service_id), 'r')
+          while (line = file.gets)
+            @logger.warn(line.chomp!)
+          end
+        rescue => e
+          @logger.warn(e)
+        ensure
+          @logger.warn(" *** END vblob log - instance: #{service_id}")
+          @logger.warn("")
+        end
+
+        def close_fds
+          3.upto(get_max_open_fd) do |fd|
+            begin
+              IO.for_fd(fd, "r").close
+            rescue
+            end
+          end
+        end
+
+        def get_max_open_fd
+          max = 0
+
+          dir = nil
+          if File.directory?("/proc/self/fd/") # Linux
+            dir = "/proc/self/fd/"
+          elsif File.directory?("/dev/fd/") # Mac
+            dir = "/dev/fd/"
+          end
+
+          if dir
+            Dir.foreach(dir) do |entry|
+              begin
+                pid = Integer(entry)
+                max = pid if pid > max
+              rescue
+              end
+            end
+          else
+            max = 65535
+          end
+
+          max
+        end
+
+      end
+    end
+  end
+end
