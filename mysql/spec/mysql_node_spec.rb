194763982765c337d802ca855f23f2b97c565211
Andrew Liu <aliu@vmware.com>
2012-04-19 11:09:18 +0800

    add configurable timeout for management mysql connection
    Change-Id: Ie5134224b219e06b5780b949505b8c23036856f7

--- mysql/spec/mysql_node_spec.rb
+++ mysql/spec/mysql_node_spec.rb
@@ -669,6 +669,24 @@ describe "Mysql server node" do
     end
   end
 
+  it "should add timeout option to all management mysql connection" do
+    EM.run do
+      opts = @opts.dup
+      timeout = 1
+      opts[:connection_wait_timeout] = timeout
+      node = VCAP::Services::Mysql::Node.new(opts)
+
+      EM.add_timer(2) do
+        node.pool.with_connection do |conn|
+          # simulate connection idle
+          sleep (timeout * 5)
+          expect{ conn.query("select 1") }.should raise_error(Mysql2::Error, /MySQL server has gone away/)
+        end
+        EM.stop
+      end
+    end
+  end
+
   after:each do
     @test_dbs.keys.each do |db|
       begin
