605b5b116d626fcc2d52acaa349e506aebfb14fa
Andrew Liu <aliu@vmware.com>
2012-05-19 03:07:41 +0800

    Add file size and redirect limitation to import_form_url job
    Change-Id: Ic50cd33d2742ea7039e21b093152c7c9685b0bee

--- base/lib/base/job/serialization.rb
+++ base/lib/base/job/serialization.rb
@@ -241,11 +241,30 @@ module VCAP::Services::Base::AsyncJob
 
       # Fetch remote uri and stream content to file.
       def fetch_url(url, file_path)
-        # TODO check the file size before download?
+        max_download_size = (@config["serialization"] && @config["serialization"]["max_download_size_mb"] || 10).to_i * 1024 * 1024 # 10M by default
+        max_redirects = @config["serialization"] && @config["serialization"]["max_download_redirects"] || 5
+
         File.open(file_path, "wb+") do |f|
           c = Curl::Easy.new(url)
+          # auto redirect
+          c.follow_location = true
+          c.max_redirects = max_redirects
+
+          c.on_header do |header|
+            k,v = header.split(/ *: */)
+            if k.downcase == 'content-length' && v.to_i > max_download_size
+              raise ServiceError.new(ServiceError::FILESIZE_TOO_LARGE, url, v.strip, max_download_size)
+            end
+
+            header.size
+          end
+
           c.on_body{|data| f.write(data)}
-          c.perform
+          begin
+            c.perform
+          rescue Curl::Err::TooManyRedirectsError
+            raise ServiceError.new(ServiceError::TOO_MUCH_REDIRECTS, url, max_redirects)
+          end
         end
       end
 
@@ -276,7 +295,7 @@ module VCAP::Services::Base::AsyncJob
             end
 
             raise "Can't find temp file: #{@temp_file_path}" unless File.exists? temp_file_path
-            raise ServiceError.new(SerivceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
+            raise ServiceError.new(ServiceError::BAD_SERIALIZED_DATAFILE, "request") unless validate_input(temp_file_path)
 
             @snapshot_id = new_snapshot_id
             @snapshot_path = get_dump_path(name, snapshot_id)
