2573f89691531ac816896434d08cd01efef65c7d
Frank Lu <flu@rbcon.com>
2012-07-07 10:00:10 -0600

    refactor import_from_data API
     1. store the uploaded file from CC and return url to CC
     2. CC will use the url to invoke import_from_url API
    
    Change-Id: I41406293e9b1ba75fbe290419ddac886c5f965c3

--- serialization_data_server/lib/server.rb
+++ serialization_data_server/lib/server.rb
@@ -7,9 +7,11 @@ require "nats/client"
 require "redis"
 require "json"
 require "sys/filesystem"
-include Sys
+require "fileutils"
+require "services/api"
+require "services/api/const"
 
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', 'mysql')
+include Sys
 
 module VCAP
   module Services
@@ -21,8 +23,10 @@ end
 class VCAP::Services::Serialization::Server < Sinatra::Base
 
   REQ_OPTS = %w(serialization_base_dir mbus port external_uri redis).map {|o| o.to_sym}
+  VALID_CREDENTIAL_CHARACTERS = ("A".."Z").to_a + ("a".."z").to_a + ("0".."9").to_a
 
   set :show_exceptions, false
+  set :method_override, true
 
   def initialize(opts)
     super
@@ -34,6 +38,8 @@ class VCAP::Services::Serialization::Server < Sinatra::Base
     @host = opts[:host]
     @port = opts[:port]
     @external_uri = opts[:external_uri]
+    @upload_token_secret = opts[:upload_token_secret] || "dlfoosecret"
+    @expire_time = opts[:expire_time] || 7200
     @router_start_channel  = nil
     @base_dir = opts[:serialization_base_dir]
     NATS.on_error do |e|
@@ -123,7 +129,7 @@ class VCAP::Services::Serialization::Server < Sinatra::Base
     logger
   end
 
-  # Unrigister external uri
+  # Unregister external uri
   def send_deactivation_notice(stop_event_loop=true)
     @nats.unsubscribe(@router_start_channel) if @router_start_channel
     @logger.debug("Unregister uri: #{@router_register_json}")
@@ -136,12 +142,196 @@ class VCAP::Services::Serialization::Server < Sinatra::Base
     "vcap:snapshot:#{service_id}"
   end
 
+  def redis_file_key(service, service_id)
+    "vcap:serialized_file:#{service}:#{service_id}"
+  end
+
   def file_path(service, id, snapshot_id, file_name)
     File.join(@base_dir, "snapshots", service, id[0,2], id[2,2], id[4,2], id, snapshot_id, file_name)
   end
 
   def nginx_path(service, id, snapshot_id, file_name)
     File.join(@nginx["nginx_path"], "snapshots", service, id[0,2], id[2,2], id[4,2], id, snapshot_id, file_name)
+  end
+
+  def upload_file_path(service, id, token, time=nil)
+    File.join(@base_dir, "uploads", service, id[0,2], id[2,2], id[4,2], id, (time||Time.now.to_i).to_s, token)
+  end
+
+  def nginx_upload_file_path(service, id, token, time=nil)
+    File.join(@nginx["nginx_path"], "uploads", service, id[0,2], id[2,2], id[4,2], id, (time||Time.now.to_i).to_s, token)
+  end
+
+  def make_file_world_readable(file)
+    begin
+      new_permission = File.lstat(file).mode | 0444
+      File.chmod(new_permission, file)
+    rescue => e
+      @logger.error("Fail to make the file #{file} world_readable.")
+    end
+  end
+
+  def get_uploaded_data_file
+
+    if @nginx
+      path = params[:data_file_path]
+      wrapper_class = Class.new do
+        attr_accessor :path
+      end
+      file = wrapper_class.new
+      file.path = path
+    else
+      file = params[:data_file]
+    end
+    file
+  end
+
+  def generate_file_token(service, service_id, file_name, length=12)
+    prefix=Array.new(length) { VALID_CREDENTIAL_CHARACTERS[rand(VALID_CREDENTIAL_CHARACTERS.length)] }.join
+    appendix=Digest::MD5.hexdigest(@upload_token_secret+service+service_id+file_name+(Time.now.to_i.to_s))
+    return prefix+appendix
+  end
+
+  def generate_download_url(service, service_id, token)
+    url = "http://#{@external_uri}/serialized/#{service}/#{service_id}/serialized/file?token=#{token}"
+  end
+
+  def register_file(service, service_id, token, file_path)
+    @redis.hset(redis_file_key(service, service_id), token, {"file" => file_path, "time" => Time.now.to_i}.to_json )
+  end
+
+  def get_file(service, service_id, token)
+    @redis.hget(redis_file_key(service, service_id), token)
+  end
+
+  def try_unregister_file(service, service_id, token, greedy=false)
+    file_info_s = get_file(service, service_id, token)
+    file = nil
+    time = nil
+    if file_info_s
+      begin
+        file_info = JSON.parse(file_info_s)
+        file = file_info["file"]
+        time = file_info["time"]
+        if file && time && (greedy == true || (@expire_time > 0 && (Time.now.to_i - time.to_i) > @expire_time))
+          @redis.hdel(redis_file_key(service, service_id), token)
+          FileUtils.rm_rf(file)
+          file = nil
+          time = nil
+        end
+      rescue => e
+        file = nil
+        # if we met exception when deleting file, keep time not nil to figure out
+      end
+    end
+    [file, time]
+  end
+
+  def authorized?
+    request_header(VCAP::Services::Api::SDS_UPLOAD_TOKEN_HEADER) == @upload_token_secret
+  end
+
+  def request_header(header)
+    # This is pretty ghetto but Rack munges headers, so we need to munge them as well
+    rack_hdr = "HTTP_" + header.upcase.gsub(/-/, '_')
+    env[rack_hdr]
+  end
+
+  # store the uploaded file
+
+  put "/serialized/:service/:service_id/serialized/data" do
+    error(403) unless authorized?
+    begin
+      data_file = get_uploaded_data_file
+      unless data_file && data_file.path && File.exist?(data_file.path)
+        error(400)
+      end
+      service = params[:service]
+      service_id = params[:service_id]
+      @logger.debug("Upload serialized data for service=#{service}, service_id=#{service_id}")
+      file_basename = File.basename(data_file.path)
+      file_token = nil
+      new_file_path = nil
+      loop {
+        file_token = generate_file_token(service, service_id, file_basename)
+        # mv the file to the upload file
+        new_file_path = upload_file_path(service, service_id, file_token)
+        break unless File.exist?(new_file_path)
+      }
+      unless new_file_path && FileUtils.mkdir_p(File.dirname(new_file_path))
+        @logger.error("Failed to create directory to store the uploaded file #{data_file.path}")
+        error(400)
+      end
+      FileUtils.mv(data_file.path, new_file_path)
+      unless File.exist?(new_file_path)
+        @logger.error("Failed to move the uploaded file #{data_file.path} to the new localtion #{new_file_path}")
+        error(400)
+      end
+      # register the file into redis
+      unless file_token && register_file(service, service_id, file_token, new_file_path)
+        @logger.error("Fail to register the uploaded file #{new_file_path} to redis: #{service} #{service_id} using token #{file_token}")
+        @logger.info("Cleanup the file #{new_file_path}")
+        FileUtils.rm_rf(new_file_path) if new_file_path
+        error(400)
+      end
+      # return url
+      download_url = generate_download_url(service, service_id, file_token)
+      status 200
+      content_type :json
+      resp = {:url => download_url}
+      VCAP::Services::Api::SerializedURL.new(resp).encode
+    rescue => e
+      @logger.error("Error when store and register the uploaded file: #{e} - #{e.backtrace.join(' | ')}")
+      error(400)
+    ensure
+      FileUtils.rm_rf(data_file.path) if data_file && data_file.path && File.exist?(data_file.path)
+    end
+  end
+
+  # download uploaded data file
+
+  get "/serialized/:service/:service_id/serialized/file" do
+    # get the token of the file
+    # Is it security enough?
+    token = params[:token]
+    error(403) unless token
+    service = params[:service]
+    service_id = params[:service_id]
+    # if the file is expired, unregister and delete it
+    file, time = try_unregister_file(service, service_id, token, false)
+    @logger.debug("--file #{file} --")
+    # send out the file if the file exists
+    if file && File.exist?(file)
+      if @nginx
+        status 200
+        content_type "application/octet-stream"
+        @logger.info("Serve file using nginx: #{file}")
+        make_file_world_readable(file)
+        response["X-Accel-Redirect"] = nginx_upload_file_path(service, service_id, token, time)
+      else
+        @logger.info("Serve file: #{file}")
+        send_file(file)
+      end
+    else
+      @logger.info("Can't find uploaded file for service #{service}/service_id #{service_id} with token #{token}")
+      error(404)
+    end
+  end
+
+  delete "/serialized/:service/:service_id/serialized/file" do
+    error(403) unless authorized?
+    # get the token of the file
+    # Is it security enough?
+    token = params[:token]
+    error(403) unless token
+    service = params[:service]
+    service_id = params[:service_id]
+    file, time = try_unregister_file(service, service_id, token, true)
+    unless file || time
+      status 200
+    else
+      error(500)
+    end
   end
 
   get "/serialized/:service/:service_id/snapshots/:snapshot_id" do
@@ -166,20 +356,21 @@ class VCAP::Services::Serialization::Server < Sinatra::Base
       error(501)
     end
 
-    path = file_path(service, service_id, snapshot_id, file_name)
-    if (File.exists? path)
+    real_path = file_path(service, service_id, snapshot_id, file_name)
+    if (File.exists? real_path)
       if @nginx
         status 200
         content_type "application/octet-stream"
         path = nginx_path(service, service_id, snapshot_id, file_name)
-        @logger.info("Serve file using nginx: #{path}")
+        @logger.info("Serve file using nginx: #{real_path}")
+        make_file_world_readable(real_path)
         response["X-Accel-Redirect"] = path
       else
-        @logger.info("Serve file: #{path}")
-        send_file(path)
+        @logger.info("Serve file: #{real_path}")
+        send_file(real_path)
       end
     else
-      @logger.info("Can't find file:#{path}")
+      @logger.info("Can't find file:#{real_path}")
       error(404)
     end
   end
