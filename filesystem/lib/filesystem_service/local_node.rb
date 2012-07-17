df6d3915d59c3c113e581330adbe3d4672516f9b
felixhoo <hhu@vmware.com>
2012-07-17 20:13:51 +0000

    Add FSS service
    1. move provision/unprovision action from provisioner to node
    2. serve request from central redis server to scan disk
    3. Add dpxfs
    
    Change-Id: I7936870b7ab0da21f87f88b72ff1a33c802d400c

---
+++ filesystem/lib/filesystem_service/local_node.rb
@@ -0,0 +1,37 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+$:.unshift File.join(File.dirname(__FILE__), ".")
+require "filesystem_service/base_node"
+
+class VCAP::Services::Filesystem::LocalNode < VCAP::Services::Filesystem::BaseNode
+  def get_backend(cred=nil)
+    if cred
+      local_path = cred["internal"]["local_path"]
+      @backends.each do |backend|
+        if backend["local_path"] == local_path
+          return backend
+        end
+      end if local_path
+      return nil
+    else
+      # Simple round-robin load-balancing; TODO: Something smarter?
+      return nil if @backends == nil || @backends.empty?
+      index = @backend_index
+      @backend_index = (@backend_index + 1) % @backends.size
+      return @backends[index]
+    end
+  end
+
+  def get_instance_dir(name, backend)
+    File.join(backend["local_path"], name)
+  end
+
+  def gen_credentials(name, backend)
+    credentials = {
+      "fs_type"     => @fs_type,
+      "name"        => name,
+      "internal"    => {
+        "local_path"  => backend["local_path"]
+      }
+    }
+  end
+end
