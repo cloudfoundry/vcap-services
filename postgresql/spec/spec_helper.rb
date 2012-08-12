1b268087f426f23247d5848185544e6d002180fa
Frank Lu <flu@rbcon.com>
2012-08-12 15:28:51 +0000

    Wardenize postgresql service
    Change-Id: I8ccb4352cb6fc283336090dda2dc10c65196a54d

--- postgresql/spec/spec_helper.rb
+++ postgresql/spec/spec_helper.rb
@@ -9,13 +9,33 @@ require 'vcap_services_base'
 
 require 'postgresql_service/util'
 require 'postgresql_service/provisioner'
+
+require 'postgresql_service/warden'
+# monkey patch of wardenized node
+module VCAP::Services::Postgresql::Warden
+  alias_method :pre_send_announcement_internal_ori, :pre_send_announcement_internal
+  def pre_send_announcement_internal
+    unless @use_warden && @options[:not_start_instances]
+      pre_send_announcement_internal_ori
+    else
+      @logger.info("Not to start instances")
+    end
+  end
+end
+
 require 'postgresql_service/node'
 
-include VCAP::Services::Postgresql::Util
+require 'pry'
+require 'pry-nav'
+require 'pry-stack_explorer'
+
+module Boolean;end
+class ::TrueClass; include Boolean; end
+class ::FalseClass; include Boolean; end
 
 def getLogger()
   logger = Logger.new( STDOUT)
-  logger.level = Logger::ERROR
+  logger.level = Logger::DEBUG
   return logger
 end
 
@@ -43,8 +63,21 @@ def getNodeTestConfig()
     :max_db_conns => parse_property(config, "max_db_conns", Integer),
     :restore_bin => parse_property(config, "restore_bin", String),
     :dump_bin => parse_property(config, "dump_bin", String),
-    :db_size_overhead => parse_property(config, "db_size_overhead", Float)
+    :db_size_overhead => parse_property(config, "db_size_overhead", Float),
+    :disk_overhead => parse_property(config, "disk_overhead", Float, :disk_overhead => 0.0),
+    :use_warden => parse_property(config, "use_warden", Boolean, :optional => true, :default => false)
   }
+  if options[:use_warden]
+    warden_config = parse_property(config, "warden", Hash, :optional => true)
+    options[:use_warden] = true
+    options[:log_dir] = parse_property(warden_config, "log_dir", String)
+    options[:port_range] = parse_property(warden_config, "port_range", Range)
+    options[:image_dir] = parse_property(warden_config, "image_dir", String)
+    options[:filesystem_quota] = parse_property(warden_config, "filesystem_quota", Boolean, :optional => true)
+    options[:service_start_timeout] = parse_property(warden_config, "service_start_timeout", Integer, :optional => true, :default => 3)
+  else
+    options[:ip_route] = "127.0.0.1"
+  end
   options
 end
 
@@ -60,4 +93,20 @@ def getProvisionerTestConfig()
     :mbus => config[:mbus]
   }
   options
+end
+
+def parse_property(hash, key, type, options = {})
+  obj = hash[key]
+  if obj.nil?
+    raise "Missing required option: #{key}" unless options[:optional]
+    options[:default]
+  elsif type == Range
+    raise "Invalid Range object: #{obj}" unless obj.kind_of?(Hash)
+    first, last = obj["first"], obj["last"]
+    raise "Invalid Range object: #{obj}" unless first.kind_of?(Integer) and last.kind_of?(Integer)
+    Range.new(first, last)
+  else
+    raise "Invalid #{type} object: #{obj}" unless obj.kind_of?(type)
+    obj
+  end
 end
