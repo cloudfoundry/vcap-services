627fb12d531ee4b171134c8c9d31d4ba0fc71b5d
Andrew Liu <aliu@vmware.com>
2012-08-17 13:11:28 +0800

    fix mysql rollback snapshot function
    1. handle sepcial characters in username and password.
    2. prevent import data using root user.
    
    Change-Id: Iff9de0e4ab1802d791e5cb611fb96f44d9daf0a7

--- mysql/lib/mysql_service/job/mysql_snapshot.rb
+++ mysql/lib/mysql_service/job/mysql_snapshot.rb
@@ -7,6 +7,16 @@ require "mysql_error"
 module VCAP::Services::Mysql::Snapshot
   include VCAP::Services::Base::AsyncJob::Snapshot
 
+  module Common
+    def init_localdb(database_url)
+      DataMapper.setup(:default, database_url)
+    end
+
+    def mysql_provisioned_service
+      VCAP::Services::Mysql::Node::ProvisionedService
+    end
+  end
+
   # Dump a database into files and save the snapshot information into redis.
   class CreateSnapshotJob < BaseCreateSnapshotJob
     include VCAP::Services::Mysql::Util
@@ -39,15 +49,21 @@ module VCAP::Services::Mysql::Snapshot
   # Rollback data from snapshot files.
   class RollbackSnapshotJob < BaseRollbackSnapshotJob
     include VCAP::Services::Mysql::Util
+    include Common
 
     def execute
+      init_localdb(@config["local_db"])
+      srv = mysql_provisioned_service.get(name)
+      instance_user = srv.user
+      instance_pass = srv.password
+
       mysql_conf = @config["mysql"]
       snapshot_file_path = @snapshot_files[0]
       raise "Can't find snapshot file #{snapshot_file_path}" unless File.exists?(snapshot_file_path)
       manifest = @manifest
       @logger.debug("Manifest for snapshot: #{manifest}")
 
-      result = import_dumpfile(name, mysql_conf, mysql_conf["user"], mysql_conf["pass"], snapshot_file_path, :mysql_bin => @config["mysql_bin"], :gzip_bin => @config["gzip_bin"])
+      result = import_dumpfile(name, mysql_conf, instance_user, instance_pass, snapshot_file_path, :mysql_bin => @config["mysql_bin"], :gzip_bin => @config["gzip_bin"])
       raise "Failed execute import command to #{name}" unless result
 
       true
