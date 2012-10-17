8784ab97414c034bde98d163c2c29e588b7dd7f3
Tang Rui <rtang@vmware.com>
2012-10-17 05:18:37 -0600

    Move warden common options parsing to base
    Change-Id: I9f4d4b02194343edac5538bd7a526d4bb12e4c4b

--- ng/redis/spec/node_spec.rb
+++ ng/redis/spec/node_spec.rb
@@ -18,7 +18,7 @@ describe VCAP::Services::Redis::Node do
     @options = getNodeTestConfig
     FileUtils.mkdir_p(@options[:base_dir])
     FileUtils.mkdir_p(@options[:image_dir])
-    FileUtils.mkdir_p(@options[:redis_log_dir])
+    FileUtils.mkdir_p(@options[:service_log_dir])
     FileUtils.mkdir_p(@options[:migration_nfs])
 
     # Setup code must be wrapped in EM.run
@@ -32,7 +32,7 @@ describe VCAP::Services::Redis::Node do
   after :all do
     FileUtils.rm_f(@options[:local_db_file])
     FileUtils.rm_rf(@options[:base_dir])
-    FileUtils.rm_rf(@options[:redis_log_dir])
+    FileUtils.rm_rf(@options[:service_log_dir])
     FileUtils.rm_rf(@options[:image_dir])
     FileUtils.rm_rf(@options[:migration_nfs])
   end
