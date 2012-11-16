a4f3166305bd375038d05ca2870e27dc05e1e950
Chunjie <czhu@vmware.com>
2012-11-16 01:12:45 -0800

    Add start/stop handler for mongo proxy in vcap-services
    Change-Id: Ia6fb7629b018eef4e1ae7f106cb7eec6ade228ad

--- ng/mongodb/lib/mongodb_service/mongodb_node.rb
+++ ng/mongodb/lib/mongodb_service/mongodb_node.rb
@@ -349,6 +349,7 @@ class VCAP::Services::MongoDB::Node::ProvisionedService
   MONGO_TIMEOUT = 2
 
   SERVICE_PORT = 27017
+  PROXY_PORT   = 29017
 
   class << self
     def init(args)
@@ -480,8 +481,8 @@ class VCAP::Services::MongoDB::Node::ProvisionedService
 
   def start_options
     options = super
-    options[:start_script] = {:script => "warden_service_ctl start #{version} #{mongod_exe_options}", :use_spawn => true}
-    options[:service_port] = SERVICE_PORT
+    options[:start_script] = {:script => "warden_service_ctl start #{adminpass} #{version} #{mongod_exe_options}", :use_spawn => true}
+    options[:service_port] = PROXY_PORT
     options
   end
 
