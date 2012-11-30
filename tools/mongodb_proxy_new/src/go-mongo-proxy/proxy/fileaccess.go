a82cb8cd564f7302e8e9f04e30d3c23c36bb82ff
Chunjie <czhu@vmware.com>
2012-11-30 02:02:24 -0800

    [services-ng] Fill up the data file number monitor handler.
      topic: new mongodb proxy
    
    Change-Id: I6891df57be552450d49ad23f44e2cf970f26222c

---
+++ tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/fileaccess.go
@@ -0,0 +1,80 @@
+package proxy
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"syscall"
+)
+
+// If -1 returns then it means something wrong.
+func iterateDatafile(dbname string, dirpath string, dbfiles map[string]int) int {
+	filecount := 0
+
+	expr := fmt.Sprintf("^%s\\.[0-9]+", dbname)
+	re, err := regexp.Compile(expr)
+	if err != nil {
+		logger.Error("Failed to compile regexp error: [%s].", err)
+		return -1
+	}
+
+	visit_file := func(path string, f os.FileInfo, err error) error {
+		if err == nil && !f.IsDir() && re.Find([]byte(f.Name())) != nil {
+			dbfiles[f.Name()] = 1
+			filecount++
+		}
+		return nil
+	}
+	filepath.Walk(dirpath, visit_file)
+	return filecount
+}
+
+func parseInotifyEvent(dbname string, buffer []byte, filecount *int, dbfiles map[string]int) error {
+	var event syscall.InotifyEvent
+	var filename string
+
+	expr := fmt.Sprintf("^%s\\.[0-9]+", dbname)
+	re, err := regexp.Compile(expr)
+	if err != nil {
+		logger.Error("Failed to compile regexp error: [%s].", err)
+		return err
+	}
+
+	index := 0
+	for index < len(buffer) {
+		err := binary.Read(bytes.NewBuffer(buffer[0:len(buffer)]), binary.LittleEndian, &event)
+		if err != nil {
+			logger.Error("Failed to do binary read inotify event: [%s].", err)
+			return err
+		}
+
+		start := index + syscall.SizeofInotifyEvent
+		end := start + int(event.Len)
+
+		// Trim the tailing 'null' byte
+		filename = strings.Trim(string(buffer[start:end]), string(0x0))
+		if re.Find([]byte(filename)) != nil {
+			logger.Debug("Get filename from inotify event: [%s].", filename)
+			switch event.Mask {
+			case syscall.IN_CREATE:
+				fallthrough
+			case syscall.IN_OPEN:
+				fallthrough
+			case syscall.IN_MOVED_TO:
+				if _, ok := dbfiles[filename]; !ok {
+					*filecount++
+					dbfiles[filename] = 1
+				}
+			case syscall.IN_DELETE:
+				*filecount--
+				delete(dbfiles, filename)
+			}
+		}
+		index = end
+	}
+	return nil
+}
