13cd97d25db90fe9845f5712739f94aed2393f76
Chunjie <czhu@vmware.com>
2012-12-01 00:28:19 -0800

    enhance proxy goroutine control
      1. add SIGTERM signal handler
      2. wait goroutine to finish when proxy are ready to exit
      3. add a simple session manager
    
      topic: new mongodb proxy
    
    Change-Id: I602ecdbda7ce8b989769418ec74993c4232acb38

--- tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/session.go
+++ tools/mongodb_proxy_new/src/go-mongo-proxy/proxy/session.go
@@ -1,8 +1,10 @@
 package proxy
 
 import (
+	"errors"
 	"io"
 	"net"
+	"sync"
 )
 
 /*
@@ -12,22 +14,64 @@ import (
  */
 const BUFFER_SIZE = 64 * 1024
 
+var errClosing = errors.New("use of closed network connection")
+
 type Session interface {
+	Reset(*net.TCPConn, *net.UnixConn, Filter)
+	GetSid() int32
 	Process()
-	Shutdown()
+	WaitForFinish()
+}
+
+type SessionManager interface {
+	NewSession(*net.TCPConn, *net.UnixConn, Filter) Session
+	WaitAllFinish()
+	MarkIdle(Session)
 }
 
 type ProxySessionImpl struct {
+	manager SessionManager
+
+	sid            int32
 	clientconn     *net.TCPConn
-	serverconn     *net.TCPConn
+	serverconn     *net.UnixConn
 	filter         Filter
-	clientshutdown chan bool
-	servershutdown chan bool
+	clientshutdown chan byte
+	servershutdown chan byte
+
+	// goroutine wait channel
+	lock    sync.Mutex
+	running uint32
+	wait    chan byte
+}
+
+// A simple session manager
+type ProxySessionManagerImpl struct {
+	actives map[int32]Session // active sessions
+	idles   map[int32]Session // idle sessions
+
+	sid  int32 // session id allocator, currently int32 length is enough
+	lock sync.Mutex
 }
 
 func (session *ProxySessionImpl) Process() {
 	go session.ForwardClientMsg()
 	go session.ForwardServerMsg()
+}
+
+func (session *ProxySessionImpl) Reset(clientfd *net.TCPConn, serverfd *net.UnixConn, f Filter) {
+	// session id will never change after allocation
+	session.clientconn = clientfd
+	session.serverconn = serverfd
+	session.filter = f
+	session.clientshutdown = make(chan byte, 1)
+	session.servershutdown = make(chan byte, 1)
+	session.running = 0
+	session.wait = make(chan byte, 1)
+}
+
+func (session *ProxySessionImpl) GetSid() int32 {
+	return session.sid
 }
 
 func (session *ProxySessionImpl) ForwardClientMsg() {
@@ -38,6 +82,10 @@ func (session *ProxySessionImpl) ForwardClientMsg() {
 	current_pkt_op = OP_UNKNOWN
 	current_pkt_remain_len = 0
 	nstart = 0
+
+	session.lock.Lock()
+	session.running++
+	session.lock.Unlock()
 
 	clientfd := session.clientconn
 	serverfd := session.serverconn
@@ -61,7 +109,7 @@ func (session *ProxySessionImpl) ForwardClientMsg() {
 				logger.Debug("TCP session with mongodb client will be closed soon.")
 				break
 			}
-			logger.Debug("TCP read from client error: [%v].", err)
+			logger.Error("TCP read from client error: [%v].", err)
 			continue
 		}
 
@@ -84,7 +132,7 @@ func (session *ProxySessionImpl) ForwardClientMsg() {
 
 		// filter process
 		if filter.FilterEnabled() && !filter.PassFilter(current_pkt_op) {
-			logger.Debug("TCP session with mongodb client is blocked by filter.")
+			logger.Error("TCP session with mongodb client is blocked by filter.")
 			break
 		}
 
@@ -97,10 +145,13 @@ func (session *ProxySessionImpl) ForwardClientMsg() {
 		nwrite, err := serverfd.Write(buffer[0:(nstart + nread)])
 		if err != nil {
 			if err == io.ErrUnexpectedEOF {
-				logger.Debug("TCP session with mongodb server encounter unexpected EOF: [%v].", err)
+				logger.Debug("UnixSocket session with mongodb server encounter unexpected EOF: [%v].", err)
+				break
+			} else if err.Error() == errClosing.Error() {
+				logger.Debug("UnixSocket session is closed: [%v].", err)
 				break
 			}
-			logger.Debug("TCP write to server error: [%v].", err)
+			logger.Error("UnixSocket write to server error: [%v].", err)
 			continue
 		}
 
@@ -120,13 +171,26 @@ func (session *ProxySessionImpl) ForwardClientMsg() {
 
 	// TCP connection half disconnection
 	clientfd.CloseRead()
-	serverfd.CloseWrite()
+	// UnixSocket disconnection
+	serverfd.Close()
+
+	session.lock.Lock()
+	session.running--
+	if session.running == 0 {
+		session.wait <- 's'
+		session.manager.MarkIdle(session)
+	}
+	session.lock.Unlock()
 
 	logger.Debug("ForwardClientMsg go routine exits.")
 }
 
 func (session *ProxySessionImpl) ForwardServerMsg() {
 	buffer := make([]byte, BUFFER_SIZE)
+
+	session.lock.Lock()
+	session.running++
+	session.lock.Unlock()
 
 	clientfd := session.clientconn
 	serverfd := session.serverconn
@@ -141,10 +205,13 @@ func (session *ProxySessionImpl) ForwardServerMsg() {
 		nread, err := serverfd.Read(buffer)
 		if err != nil {
 			if err == io.EOF {
-				logger.Debug("TCP session with mongodb server will be closed soon.")
+				logger.Debug("UnixSocket session with mongodb server will be closed soon.")
+				break
+			} else if err.Error() == errClosing.Error() {
+				logger.Debug("UnixSocket session is closed: [%v].", err)
 				break
 			}
-			logger.Debug("TCP read from server error: [%v].", err)
+			logger.Error("UnixSocket read from server error: [%v].", err)
 			continue
 		}
 
@@ -154,28 +221,114 @@ func (session *ProxySessionImpl) ForwardServerMsg() {
 				logger.Debug("TCP session with mongodb client encounter unexpected EOF: [%v].", err)
 				break
 			}
-			logger.Debug("TCP write to client error: [%v].", err)
+			logger.Error("TCP write to client error: [%v].", err)
 			continue
 		}
 	}
 
+	// UnixSocket disconnection
+	serverfd.Close()
 	// TCP connection half disconnection
-	serverfd.CloseRead()
 	clientfd.CloseWrite()
+
+	session.lock.Lock()
+	session.running--
+	if session.running == 0 {
+		session.wait <- 's'
+		session.manager.MarkIdle(session)
+	}
+	session.lock.Unlock()
 
 	logger.Debug("ForwardServerMsg go routine exits.")
 }
 
-func (session *ProxySessionImpl) Shutdown() {
-	session.clientshutdown <- true
-	session.servershutdown <- true
+func (session *ProxySessionImpl) WaitForFinish() {
+	session.clientshutdown <- 's'
+	session.servershutdown <- 's'
+	wait := false
+	session.lock.Lock()
+	if session.running > 0 {
+		wait = true
+	}
+	session.lock.Unlock()
+	if wait {
+		<-session.wait
+	}
 }
 
-func NewSession(clientfd *net.TCPConn, serverfd *net.TCPConn, f Filter) *ProxySessionImpl {
+func (manager *ProxySessionManagerImpl) NewSession(clientfd *net.TCPConn, serverfd *net.UnixConn, f Filter) Session {
+	var session Session
+	var sid int32
+
+	sid = -1
+	manager.lock.Lock()
+	for sid, session = range manager.idles {
+		break
+	}
+	if sid >= 0 {
+		delete(manager.idles, sid)
+	}
+	manager.lock.Unlock()
+
+	if sid >= 0 {
+		session.Reset(clientfd, serverfd, f)
+	} else {
+		session = manager.SpawnSession(clientfd, serverfd, f)
+	}
+
+	manager.lock.Lock()
+	manager.actives[sid] = session
+	manager.lock.Unlock()
+
+	return session
+}
+
+func (manager *ProxySessionManagerImpl) WaitAllFinish() {
+	temp := make(map[int32]Session)
+
+	manager.lock.Lock()
+	for sid, session := range manager.idles {
+		temp[sid] = session
+	}
+	manager.lock.Unlock()
+
+	for _, session := range temp {
+		session.WaitForFinish()
+	}
+}
+
+func (manager *ProxySessionManagerImpl) MarkIdle(session Session) {
+	manager.lock.Lock()
+	if sid := session.GetSid(); sid >= 0 {
+		delete(manager.actives, sid)
+		manager.idles[sid] = session
+	}
+	manager.lock.Unlock()
+}
+
+func (manager *ProxySessionManagerImpl) SpawnSession(clientfd *net.TCPConn, serverfd *net.UnixConn, f Filter) Session {
+	var sid int32
+
+	manager.lock.Lock()
+	sid = manager.sid
+	manager.sid++
+	manager.lock.Unlock()
+
 	return &ProxySessionImpl{
+		manager:        manager,
+		sid:            sid,
 		clientconn:     clientfd,
 		serverconn:     serverfd,
 		filter:         f,
-		clientshutdown: make(chan bool, 1),
-		servershutdown: make(chan bool, 1)}
+		clientshutdown: make(chan byte, 1),
+		servershutdown: make(chan byte, 1),
+		running:        0,
+		wait:           make(chan byte, 1)}
+}
+
+func NewSessionManager() *ProxySessionManagerImpl {
+	return &ProxySessionManagerImpl{
+		actives: make(map[int32]Session),
+		idles:   make(map[int32]Session),
+		sid:     0}
 }
