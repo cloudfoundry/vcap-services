7e3294478413ffd1728521c08e6246a6ec4b76f7
seanzhao <seanzhao@rbcon.com>
2012-12-13 11:27:47 +0800

    add a new process to cleanup the service redis periodically
    Change-Id: I62e6f8930fd69fbd7d6bae366bea9f0a69861388

---
+++ tools/backup/manager/lib/backup_manager/job_cleaner.rb
@@ -0,0 +1,44 @@
+require 'json'
+require 'json_message'
+require 'vcap_services_base'
+
+module VCAP
+  module Services
+    module Backup
+    end
+  end
+end
+
+
+class VCAP::Services::Backup::JobCleaner
+  include VCAP::Services::Base::AsyncJob
+  include VCAP::Services::Backup::Util
+  def initialize(manager, options, services_redis)
+    @manager = manager
+    @manager.logger.info("#{self.class}: Initializing")
+    @options = options
+    VCAP::Services::Base::AsyncJob::Config.redis_config = services_redis
+  end
+
+  def run
+    @manager.logger.info("#{self.class}: Running. Scanning services redis")
+    scan
+  end
+
+  def scan
+    expiration = n_midnights_ago(@options[:max_days])
+    get_all_jobs.each do |jid|
+      job = get_job(jid)
+      expired = Time.parse(job[:start_time]).to_i < expiration
+      remove_job(jid) if expired
+      if job["status"] == "failed"
+        @manager.logger.info("Job failed and expired, cleaning up job [#{jid}]: #{job.inspect}")
+      else
+        @manager.logger.info("Job expired, cleaning up job [#{jid}]: #{job.inspect}")
+      end
+    end
+  rescue Exception => x
+    @manager.logger.error("#{self.class}: Exception while running: #{x.message}, #{x.backtrace.join(', ')}")
+  end
+end
+
